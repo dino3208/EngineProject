f243d3b93cbde9262eef3d64faaebfc219b5c9c5
dino3208 <dino3208@naver.com>
2026-07-30 17:01:16 +0900

    개발 1일차

---
+++ CraftEngine/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Engine/Engine.h>
+
+// #include "Engine/Engine.h" 
+// <>보다 ""의 검색범위가 더 넓다.
+
+int main()
+{
+	// 엔진 객체 생성 및 실행
+	Craft::Engine engine;
+	engine.Run();
+}
