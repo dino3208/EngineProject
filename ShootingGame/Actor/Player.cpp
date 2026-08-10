b6b29796171ddb06f3b78fc8cfef4a6e739b05c2
dino3208 <dino3208@naver.com>
2026-08-10 16:03:38 +0900

    총알 생성

--- ShootingGame/Actor/Player.cpp
+++ ShootingGame/Actor/Player.cpp
@@ -1,10 +1,13 @@
 #include "Player.h"
 #include <Engine/Engine.h>
 #include <Input/Input.h>
+#include <Level/Level.h>
+#include <Actor/PlayerBullet.h>
 
 using namespace Craft;
 Player::Player()
-	: Actor("<=A=>", Vector2::Zero, Color::Green)
+	: Actor("<=A=>", Vector2::Zero, Color::Green),
+	fireMode(FireMode::OneShot)
 {
 	// 생성 위치 설정
 	int x = (Engine::Get().GetWidth() / 2) - (width / 2);
@@ -13,6 +16,9 @@ Player::Player()
 
 	// x 위치 저장
 	xPosition = static_cast<float>(x);
+
+	// 연사 타이머 시간 설정
+	timer.SetTargetTime(fireInterval);
 }
 
 
@@ -40,6 +46,39 @@ void Player::Tick(float deltaTime)
 
 	// 이동 함수 호출
 	Move(direction, deltaTime);
+
+	// 발사 타이머 업데이트
+	timer.Tick(deltaTime);
+
+	if (fireMode == FireMode::OneShot)
+	{
+		// 탄약 발사 처리
+		if (Input::Get().GetKeyDown(VK_SPACE))
+		{
+			Fire();
+		}
+	}
+	else if (fireMode == FireMode::Repeat)
+	{
+		if (Input::Get().GetKey(VK_SPACE))
+		{
+			// 연사 발사 함수 호출
+			FireInterval();
+		}
+	}
+
+	// 발사 모드 전환 처리
+	if (Input::Get().GetKeyDown('R'))
+	{
+		if (fireMode == FireMode::OneShot)
+		{
+			fireMode = FireMode::Repeat;
+		}
+		else if (fireMode == FireMode::Repeat)
+		{
+			fireMode = FireMode::OneShot;
+		}
+	}
 }
 
 void Player::Move(float direction, float deltaTime)
@@ -68,4 +107,37 @@ void Player::Move(float direction, float deltaTime)
 	// float 값을 int로 형변환할 때 소숫점 값은 버림 처리된다는 점 주의
 	newPosition.x = static_cast<int>(xPosition);
 	SetPosition(newPosition);
+}
+
+void Player::Fire()
+{
+	// 탄약 생성 위치 구하기
+	// 플레이어의 가운데 위치
+	// <=A=>
+	//   ^ 
+	Vector2 bulletPosition(
+		GetPosition().x + (width / 2),
+		GetPosition().y);
+
+
+	// 탄약 생성
+	std::shared_ptr<Level> owner = GetOwner();
+	if (owner)
+	{
+		owner->SpawnActor<PlayerBullet>(bulletPosition);
+	}
+}
+void Player::FireInterval()
+{
+	// 발사 가능 여부 확인
+	if (!CanShoot())
+	{
+		return;
+	}
+
+	// 발사 처리
+	Fire();
+
+	// 경과 시간 초기화
+	timer.Reset();
 }
--- ShootingGame/Actor/Player.h
+++ ShootingGame/Actor/Player.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <Actor/Actor.h>
+#include <Util/Timer.h>
 
 // 좌우로 디오/스페이스 키로 탄약을 발사하는 플에이어
 
 class Player : public Craft::Actor
 {
+	// 발사 모드 (단발/연사)
+	enum class FireMode
+	{
+		None = -1,
+		OneShot,
+		Repeat
+	};
+
 	// 커스텀 타입 등록
 	TYPE_DECLARATIONS(Player, Actor)
 
@@ -19,11 +28,29 @@ private:
 	// 이동 처리 함수
 	void Move(float direction, float deltaTime);
 
+	// 탄약 발사 함수
+	void Fire();
+
+	// 연속 발사 함수
+	void FireInterval();
+
+	// 발사 가능 여부 확인 함수
+	inline bool CanShoot() const { return timer.IsTimeOut(); }
+
 private:
 	// 이동 처리에 필요한 변수
 	float xPosition = 0.0f;
 
 	// 이동 속도 변수
 	float moveSpeed = 70.0f;
+
+	// 발사 모드 변수
+	FireMode fireMode = FireMode::None;
+
+	// 타이머 변수
+	Timer timer;
+
+	// 연사시 발사 간격 (단위: 초)
+	float fireInterval = 0.05f;
 };
 
