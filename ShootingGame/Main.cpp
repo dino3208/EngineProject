f7f68034d5a064fe418c705de7f056f4c9eb006b
dino3208 <dino3208@naver.com>
2026-08-25 21:09:28 +0900

    Lesson1:각도/삼각함수

--- ShootingGame/Main.cpp
+++ ShootingGame/Main.cpp
@@ -1,39 +1,12 @@
-﻿#include <Engine/Engine.h>
-
+﻿#include <cmath>
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <string>
-
-const std::vector<std::string> mapData =
-{
-	"########",
-	"#......#",
-	"#..##..#",
-	"#......#",
-	"########"
-};
-
-bool IsWall(int x, int y)
-{
-	if (y < 0 || y >= (int)mapData.size())
-	{
-		return true;
-	}
-	if (x < 0 || x >= (int)mapData[y].length())
-	{
-		return true;
-	}
-
-	return mapData[y][x] == '#';
-}
 
 int main()
 {
-	int playerX = 3;
-	int playerY = 2;
+	float playerAngle = 0.0f;
 
-	bool isWall = IsWall(playerX, playerY);
+	float dx = cosf(playerAngle);
+	float dy = sinf(playerAngle);
 
-	std::cout << "isWall: " << isWall << std::endl;
+	std::cout << "dx: " << dx << ", dy: " << dy << std::endl;
 }
