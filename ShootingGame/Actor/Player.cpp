71a37a8d3e856561d8fe460f04df10588c125b51
dino3208 <dino3208@naver.com>
2026-08-27 08:53:25 +0900

    코드 정리 및 참조 추가

--- ShootingGame/Actor/Player.cpp
+++ ShootingGame/Actor/Player.cpp
@@ -2,6 +2,7 @@
 #include <Input/Input.h>
 #include <Render/Renderer.h>
 
+// 맵 데이터 Todo: 나중에 파일로 수정하는 형식으로 변환
 std::vector<std::string> mapData =
 {
 	"########",
@@ -11,11 +12,22 @@ std::vector<std::string> mapData =
 	"########"
 };
 
-
+// 화면 크기 *주의사항* Config\Setting.txt 파일을 수정하고 수정할 것.(일치 필수)
+// 나중에 파일 참고 형식으로 변환
 const int screenHeight = 80;
 const int screenWidth = 240;
 
+// 각도->라디안 변환 함수
+const float PI = 3.14159265f;
+float DegToRad(float degree)
+{
+	return degree * (PI / 180.0f);
+}
 
+// 시야각
+const float FOV = DegToRad(60.0f);
+
+// 벽인지 확인
 bool IsWall(int x, int y)
 {
 	if (y < 0 || y >= (int)mapData.size())
@@ -30,13 +42,11 @@ bool IsWall(int x, int y)
 	return mapData[y][x] == '#' || mapData[y][x] == 'D';
 }
 
-const float PI = 3.14159265f;
 
-float DegToRad(float degree)
-{
-	return degree * (PI / 180.0f);
-}
+// 화면 가로 넓이 갯수만큼 광선 갯수
+float distances[screenWidth];
 
+// 광선 함수
 float CastRay(float startX, float startY, float angle)
 {
 	const float step = 0.05f;
@@ -46,12 +56,17 @@ float CastRay(float startX, float startY, float angle)
 	float rayY = startY;
 	float distance = 0.0f;
 
+	// 움직이는 거리가 최대거리보다 작은 경우
 	while (distance < maxDistance)
 	{
+		// X좌표 광선
 		rayX += cosf(angle) * step;
+		// Y좌표 광선
 		rayY += sinf(angle) * step;
+		// 거리에 광선 반영
 		distance += step;
 
+		// 
 		if (IsWall((int)rayX, (int)rayY))
 		{
 			return distance;
@@ -62,9 +77,7 @@ float CastRay(float startX, float startY, float angle)
 }
 
 
-const float FOV = DegToRad(60.0f);
 
-float distances[screenWidth];
 
 //void CastAllRays(float playerX, float playerY, float playerAngle)
 //{
@@ -78,12 +91,14 @@ float distances[screenWidth];
 //	}
 //}
 
+// 광선 여러개 쏘는 함수
 void CastAllRays(float playerX, float playerY, float playerAngle)
 {
 	float angleStep = FOV / screenWidth;
 
 	for (int i = 0;i < screenWidth;++i)
 	{
+		// 비스듬히 쏘는 광선에 의해 외곡되는 광선 보정 (어안렌즈 현상 수정)
 		float offset = angleStep * i - FOV / 2.0f;
 		float rayAngle = playerAngle + offset;
 
@@ -109,6 +124,7 @@ void CastAllRays(float playerX, float playerY, float playerAngle)
 //}
 //return ' ';
 
+// 거리에 따른 벽 표시 글자 변화로 거리감 표현
 char GetWallChar(float distance)
 {
 	if (distance < 3.0f)
@@ -126,6 +142,7 @@ char GetWallChar(float distance)
 	return ' ';
 }
 
+// 거리에 따른 벽 색깔 표시
 Craft::Color GetWallColor(float distance)
 {
 	if (distance < 3.0f)
@@ -140,9 +157,27 @@ Craft::Color GetWallColor(float distance)
 	return Craft::Color::Red;
 }
 
+// 아이템 줍기 시도 함수
+void TryPickUpItem(float playerX, float playerY)
+{
+	int x = (int)playerX;
+	int y = (int)playerY;
+
+	// 화면 밖으로 나가는 것 방지
+	if (y < 0 || y >= (int)mapData.size()) { return; }
+	if (x < 0 || x >= (int)mapData[y].length()) { return; }
+
+	// 열쇠인 K 획득시. 열쇠 보유로 전환 + K를 바닥으로 전환
+	if (mapData[y][x] == 'K')
+	{
+		hasKey = true;
+		mapData[y][x] = '.';
+	}
+}
+
+// 열쇠 메커니즘
 bool hasKey = false;
-
-
+// 문 열기 시도 함수
 void TryOpenDoor(float playerX, float playerY, float playerAngle)
 {
 	if (!hasKey)
@@ -152,31 +187,18 @@ void TryOpenDoor(float playerX, float playerY, float playerAngle)
 	int frontX = (int)(playerX + cosf(playerAngle));
 	int frontY = (int)(playerY + sinf(playerAngle));
 
+	// 화면 밖으로 나가는 경우 방지
 	if (frontY < 0 || frontY >= (int)mapData.size()) { return; }
 	if (frontX < 0 || frontX >= (int)mapData[frontY].length()) { return; }
 
+	// 열쇠가 있을 경우 문인 D를 바닥인 .으로 변환
 	if (mapData[frontY][frontX] == 'D')
 	{
 		mapData[frontY][frontX] = '.';
 	}
 }
 
-void TryPickUpItem(float playerX, float playerY)
-{
-	int x = (int)playerX;
-	int y = (int)playerY;
-
-	if (y < 0 || y >= (int)mapData.size()) { return; }
-	if (x < 0 || x >= (int)mapData[y].length()) { return; }
-
-	if (mapData[y][x] == 'K')
-	{
-		hasKey = true;
-		mapData[y][x] = '.';
-	}
-}
-
-
+//---------------------------------------------------------------------------------------//
 
 using namespace Craft;
 
@@ -232,6 +254,7 @@ void Player::Tick(float deltaTime)
 
 void Player::Draw()
 {
+	// 거리에 따른 벽 위아래 그리기로 거리감 표현
 	for (int x = 0;x < screenWidth;++x)
 	{
 		int wallHeight = (int)(screenHeight / distances[x]);
@@ -240,6 +263,7 @@ void Player::Draw()
 			wallHeight = screenHeight;
 		}
 
+		// 벽 그리기 -> 콘솔의 Y좌표는 반대임에 유의
 		int wallTop = (screenHeight / 2) - (wallHeight / 2);
 		int wallBottom = (screenHeight / 2) + (wallHeight / 2);
 
@@ -254,7 +278,4 @@ void Player::Draw()
 	}
 }
 
-
-
-//---------------------------------------------------------------------------------
 
--- ShootingGame/Actor/Player.h
+++ ShootingGame/Actor/Player.h
@@ -11,10 +11,19 @@ public:
 	virtual void Draw() override;
 
 protected:
+	// 캐릭터 좌표
 	float playerX = 1.0f;
 	float playerY = 1.0f;
+
+	// 캐릭터 시야방향
 	float playerAngle = 0.0f;
 
+	// 비트 기반
+	const float bpm = 120.0f;
+	const float secondsPerBeat = 60.0f / bpm;
+	float beatTimer = 0.0f;
+
+	// 열쇠 보유 유무
 	bool hasKey = false;
 };
 
